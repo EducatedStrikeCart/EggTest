0f4f9fbb403c0599d441a8dc259a28cc3c0d8a20
EducatedStrikeCart <dhn004@gmail.com>
2022-02-12 16:24:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using BlazorSandbox;
+using BlazorSandbox.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<EggService>();
+builder.Services.AddBlazoredLocalStorage();
+
+await builder.Build().RunAsync();
---
+++ Services/EggService.cs
@@ -0,0 +1,36 @@
+﻿using Blazored.LocalStorage;
+
+namespace BlazorSandbox.Services
+{
+	public class EggService
+	{
+		public event Action OnChange;
+
+		private readonly ILocalStorageService _localStorageService;
+
+		public EggService(ILocalStorageService localStorageService)
+		{
+			this._localStorageService = localStorageService;
+		}
+
+		public async Task<int> GetEggs()
+		{
+			int currentEggs =  await _localStorageService.GetItemAsync<int>("Eggs");
+			return currentEggs;
+		}
+
+		public async Task AddEgg()
+		{
+			int newEggs = await GetEggs();
+			if (newEggs == null)
+			{
+				newEggs = 0;
+			} else
+            {
+				newEggs += 1;
+            }
+			await _localStorageService.SetItemAsync("Eggs", newEggs);
+			OnChange?.Invoke();
+		}
+	}
+}
